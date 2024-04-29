b281e45cfd3b2eb84c35a01ccab559c69902ffcb
박진아 <fdcwrt@gmail.com>
2024-04-29 18:51:29 +0900

    2024

--- python/coffee/tomntomCoffee.py
+++ python/coffee/tomntomCoffee.py
@@ -12,27 +12,23 @@ import json
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
-filename = f"chart_flo100_{current_date}.json"
-
-# 웹드라이브 설치(수동)
-# browser = webdriver.Chrome()
-# browser.get("https://www.music-flo.com/browse")
+filename = f"menu-tomntom_{current_date}.json"
 
 # 웹드라이브 설치
 options = ChromeOptions()
 service = ChromeService(executable_path=ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service, options=options)
-browser.get("https://www.music-flo.com/browse")
+browser.get("https://www.tomntoms.com/menu/drink")
 
 # 페이지가 완전히 로드될 때까지 대기
 WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "chart_lst"))
+    EC.presence_of_element_located((By.CLASS_NAME, "pb-20"))
 )
 
 # "더보기" 버튼을 찾아 클릭
 try:
     more_button = WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn_list_more"))
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".custom-button.main-tab"))
     )
     if more_button:
         browser.execute_script("arguments[0].click();", more_button)
@@ -41,32 +37,25 @@ try:
 except Exception as e:
     print("Error clicking '더보기':", e)
 
-
 # 업데이트된 페이지 소스를 변수에 저장
 html_source_updated = browser.page_source
 soup = BeautifulSoup(html_source_updated, 'html.parser')
 
 # 데이터 추출
-music_data = []
-tracks = soup.select(".chart_lst .track_list_table tbody tr")
-for track in tracks:
-    rank = track.select_one(".num").text.strip()
-    title = track.select_one(".tit__text").text.strip()
-    artist = track.select_one(".artist__link").text.strip()
-    album = track.select_one(".album").text.strip()
-    image_url = track.select_one(".thumb img").get('data-src')
+coffee_data = []
+tracks = soup.select("#root > section.max-w-7xl.p-4.mx-auto.pb-20.w-full > div.grid.gap-6.mt-8.grid-cols-1 .relative.w-full")
 
-    music_data.append({
-        "rank": rank,
+for track in tracks:
+    title = track.select_one(".relative.w-full button > div > div > p > span.tracking-wider").text.strip()    
+    image_url = track.select_one(".relative.w-full button > div > img").get('src')
+    coffee_data.append({
         "title": title,
-        "artist": artist,
         "imageURL": image_url,
-        "album": album
     })
 
 # 데이터를 JSON 파일로 저장
 with open(filename, 'w', encoding='utf-8') as f:
-    json.dump(music_data, f, ensure_ascii=False, indent=4)
+    json.dump(coffee_data, f, ensure_ascii=False, indent=4)
 
-# 브라우저 종료
-browser.quit()
+# # 브라우저 종료
+# browser.quit()

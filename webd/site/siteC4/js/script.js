04be6780b25b923d3d261374669659afd550ce9c
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-12 01:54:19 +0900

    2024

--- webd/site/siteC4/js/script.js
+++ webd/site/siteC4/js/script.js
@@ -1,33 +1,31 @@
 $(function () {
-    // 이미지 슬라이드
-    let currentIndex = 0;   //현재 이미지 설정
-    $(".sliderWrap").append($(".slider").first().clone(true)); //첫번째 이미지를 복사해서 마지막에 추가
+    //슬라이드
+    let currentIndex = 0;
+    $(".sliderWrap").append($(".slider").first().clone(true));
 
-    setInterval(function () {     //3초에 한번씩 실행
-        currentIndex++;     //현재 이미지를 1씩 증가
+    setInterval(() => {
+        currentIndex++;
         $(".sliderWrap").animate({ marginTop: -350 * currentIndex + "px" }, 600);
 
-        if (currentIndex == 3) {  //마지막 이미지가 됐을 때
-            setTimeout(function () {  //한번만 실행
-                $(".sliderWrap").animate({ marginTop: 0 }, 0); //애니메이션을 정지
-                currentIndex = 0;   //현재 이미지를 초기화
-            }, 700);
+        if (currentIndex == 3) {
+            setTimeout(() => {
+                $(".sliderWrap").animate({ marginTop: 0 }, 0);
+                currentIndex = 0;
+            }, 600)
         }
-    }, 3000);
-
-    // 메뉴
-    $(".nav > ul > li").mouseover(function () {
-        $(this).find(".submenu").stop().slideDown();
-    });
-    $(".nav > ul > li").mouseout(function () {
-        $(this).find(".submenu").stop().slideUp();
-    });
-
-    // 팝업
-    $(".popup-btn").click(function () {
-        $(".popup-view").show();
-    });
-    $(".popup-close").click(function () {
-        $(".popup-view").hide();
-    });
-});
+    }, 3000)
+    //메뉴
+    $(".nav>ul>li").mouseover(function () {
+        $(this).find(">ul").stop().slideDown();
+    })
+    $(".nav>ul>li").mouseout(function () {
+        $(this).find(">ul").stop().slideUp();
+    })
+    //팝업
+    $(".popup_btn").click(function () {
+        $(".popup_view").show();
+    })
+    $(".popup_close").click(function () {
+        $(".popup_view").hide();
+    })
+})
